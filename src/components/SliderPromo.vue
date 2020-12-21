0c7108732b2f51dc0215f7188415c0e1ae07986a
FGE <lpablo@fiscaliaveracruz.gob.mx>
2020-12-21 12:32:34 -0600

    first version of my main page

---
+++ src/components/SliderPromo.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-carousel>
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+      fluid fill-height
+      style="max-height: 100vh;"
+    ></v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+  export default {
+    name: 'SliderPromo',
+    data () {
+      return {
+        items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          },
+        ],
+      }
+    },
+  }
+</script>
